b9bce401809c5feca3f1dd18aacaf50aa652755a
Yash007 <sompurayash@live.com>
2018-05-23 13:37:22 -0400

    Users class for MVC

--- Model/Users.php
+++ Model/Users.php
@@ -1,15 +1,11 @@
 <?php
-
 	/*
 	 * Project Name: Carpool Webservice
 	 * Organized and Created by: Yash sompura
 	 * License: MIT License
 	 * Date: 23 May, 2018
 	 * File Name: index.php
+	 * Model Class
 	 */
 
-	// Rounting URL Links
-	
-	// Select the Controller class for Manipulation of data
-	
 ?>
